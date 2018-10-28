01af1856e920c877861153644459737c64fbce97
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-10-27 17:34:59 -0700

    Reverted back to Aug 11 '18 edit

--- basicBot.js
+++ basicBot.js
@@ -1,10 +1,7 @@
 /**
-
  Copyright © 2014-2018 basicBot
-
  Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
-
  */
 
 (function() {
@@ -64,7 +61,6 @@
             $.getScript('https://cdn.jsdelivr.net/sockjs/1.0.3/sockjs.min.js', loadSocket);
         } else loadSocket();
     }
-
     var sendToSocket = function() {
         var basicBotSettings = basicBot.settings;
         var basicBotRoom = basicBot.room;
@@ -196,6 +192,7 @@
                 }
             });
         }
+
     };
 
     String.prototype.splitBetween = function(a, b) {
@@ -1558,7 +1555,6 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-
                     }
                 }
             },
@@ -2238,18 +2234,15 @@
                         for (var i = 0; i < chats.length; i++) {
                             var n = from[i].textContent;
                             if (name.trim() === n.trim()) {
-
                                 // var messagecid = $(message)[i].getAttribute('data-cid');
                                 // var emotecid = $(emote)[i].getAttribute('data-cid');
                                 // API.moderateDeleteChat(messagecid);
-
                                 // try {
                                 //     API.moderateDeleteChat(messagecid);
                                 // }
                                 // finally {
                                 //     API.moderateDeleteChat(emotecid);
                                 // }
-
                                 if (typeof $(message)[i].getAttribute('data-cid') == 'undefined'){
                                     API.moderateDeleteChat($(emote)[i].getAttribute('data-cid')); // works well with normal messages but not with emotes due to emotes and messages are seperate.
                                 } else {
